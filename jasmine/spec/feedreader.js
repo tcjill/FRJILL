dfcedd20bf66506bf3e929d7caa5a859b3aa8710
jill <jgundaker@gmail.com>
2018-07-07 18:07:20 -0700

    menu hidden/changes visability display and hide again

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -50,23 +50,38 @@ $(function() {
             })
         })
     });
-
-describe('The menu', function(){})
-    /* TODO: Write a new test suite named "The menu" */
+        /* TODO: Write a new test suite named "The menu" */
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
+        
          */
-
-         /* TODO: Write a test that ensures the menu changes
+describe('The menu', function(){})
+        /* TODO: Write a test that ensures the menu element is
+         * hidden by default. You'll have to analyze the HTML and
+         * the CSS---Go into the file and look for them-- to determine how we're performing the
+         * hiding/showing of the menu element.*/
+        it('element is hidden by default',function(){
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+        /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
 
+        it ('menu displays when clicked and hides when clicked again', function(){
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+
+      
+    
+       
+
     /* TODO: Write a new test suite named "Initial Entries" */
+describe('Initial Entries', function(){})
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
